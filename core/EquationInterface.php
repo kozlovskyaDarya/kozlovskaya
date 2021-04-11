5bd7c5df3baa88900029e7099c4ce3b59d0891ab
kozlovskayaDarya <daryakozlovskaya1999@yahoo.com>
2021-04-11 22:27:36 +0300

    lab3

---
+++ core/EquationInterface.php
@@ -0,0 +1,7 @@
+<?php namespace core;
+	
+interface EquationInterface {
+    public function qu_solve($a, $b, $c);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,50 +1,14 @@
 <?php
 
-class A
-{
-	function slo($a, $b)
-	{
-		if($a==0)
-		{
-			return null;
-		}
-	return $this->x==-$b/$a;
-	}
-	protected $x;
-}
+use kozlovskaya\kozlovskayaException;
+use kozlovskaya\QuEquation;
+use kozlovskaya\MyLog;
 
-class B extends A
-{
-	function long($a, $b, $c)
-	{
-		if ($a==0) return parent::line ($b,$c);
-		$d=$this->discriminant ($a, $b, $c);
-
-		if($d==0)
-		{
-			return $this->x=array ((-$b)/(2*$a));
-		}
-
-		if($d>0)
-		{
-			return $this->x=array ((-$b)/(2*$a),(-$b)/(2*$a));
-		}
-		return null;
-	}
-	protected function discriminant ($a, $b, $c)
-	{
-		return(($b*$b)-4*$a*$c);
-	}
-
-}
-
-$a1 = new A();
-$b2 = new B();
-$a1->slo (45, -32);
-$b2->long (28, 15, -34);
-
-var_dump ($a1);
-var_dump ($b2);
-
-
+include "core/EquationInterface.php";
+include "core/LogAbstract.php";
+include "core/LogInterface.php";
+include "kozlovskaya/MyLog.php";
+include "kozlovskaya/Equation.php";
+include "kozlovskaya/QuEquation.php";
+include "kozlovskaya/kozlovskayaException.php";
 ?> 
