5551f5f1f3b9814610eaf111f032588de40a06b1
kozlovskayaDarya <daryakozlovskaya1999@yahoo.com>
2021-04-11 22:22:21 +0300

    lab2

--- index.php
+++ index.php
@@ -2,37 +2,49 @@
 
 class A
 {
+	function slo($a, $b)
+	{
+		if($a==0)
+		{
+			return null;
+		}
+	return $this->x==-$b/$a;
+	}
+	protected $x;
 }
 
 class B extends A
 {
-	public function __construct ($b)
+	function long($a, $b, $c)
 	{
-		$this->b = $b;
+		if ($a==0) return parent::line ($b,$c);
+		$d=$this->discriminant ($a, $b, $c);
+
+		if($d==0)
+		{
+			return $this->x=array ((-$b)/(2*$a));
+		}
+
+		if($d>0)
+		{
+			return $this->x=array ((-$b)/(2*$a),(-$b)/(2*$a));
+		}
+		return null;
+	}
+	protected function discriminant ($a, $b, $c)
+	{
+		return(($b*$b)-4*$a*$c);
 	}
 
-	protected $b;
-}
-
-class C extends B
-{
-    public function __construct ($a, $b, $c)
-    {
-        $this->c = $c;
-        $this->a = $a;
-        parent::__construct($b);
-    }
-
-    protected $c;
-    protected $a;
 }
 
 $a1 = new A();
-$a2 = new A();
-$a3 = new A();
-$b4 = new B($a2);
-$c5 = new C($a1, $a3, $b4);
+$b2 = new B();
+$a1->slo (45, -32);
+$b2->long (28, 15, -34);
 
-var_dump ($c5);
+var_dump ($a1);
+var_dump ($b2);
+
 
 ?> 
