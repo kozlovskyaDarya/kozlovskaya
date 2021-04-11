78367c4a3493e41bd4431b56d7587d7263336208
kozlovskayaDarya <daryakozlovskaya1999@yahoo.com>
2021-04-11 22:30:10 +0300

    lab4

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
 <?php
 
-use kozlovskaya\kozlovskayaException;
-use kozlovskaya\QuEquation;
-use kozlovskaya\MyLog;
+class A
+{
+}
 
-include "core/EquationInterface.php";
-include "core/LogAbstract.php";
-include "core/LogInterface.php";
-include "kozlovskaya/MyLog.php";
-include "kozlovskaya/Equation.php";
-include "kozlovskaya/QuEquation.php";
-include "kozlovskaya/kozlovskayaException.php";
+class B extends A
+{
+	public function __construct ($b)
+	{
+		$this->b = $b;
+	}
+
+	protected $b;
+}
+
+class C extends B
+{
+    public function __construct ($a, $b, $c)
+    {
+        $this->c = $c;
+        $this->a = $a;
+        parent::__construct($b);
+    }
+
+    protected $c;
+    protected $a;
+}
+
+$a1 = new A();
+$a2 = new A();
+$a3 = new A();
+$b4 = new B($a2);
+$c5 = new C($a1, $a3, $b4);
+
+var_dump ($c5);
+
 ?> 
